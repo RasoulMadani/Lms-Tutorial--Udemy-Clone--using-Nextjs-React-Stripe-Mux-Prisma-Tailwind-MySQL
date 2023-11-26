fe7178eaaeed8afd70f7638505d7863cf975a8dc
Rasoulmadani <ali.rahbary@chmail.ir>
2023-11-26 11:28:21 +0330

    delete extra files

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>first page</div>;
+  return <div className="text-3xl font-medium text-red-700">first page</div>;
 }
