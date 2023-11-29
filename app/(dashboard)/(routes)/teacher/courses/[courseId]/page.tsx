77e6ce4bf6d8eafd36f76472f502aa8a9b9bdf5b
Rasoulmadani <ali.rahbary@chmail.ir>
2023-11-29 13:35:40 +0330

    add form to change title of Course

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import TitleForm from "./_components/TitleForm";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -28,23 +29,26 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const totalFields = requiredFields.length;
   const completeFields = requiredFields.filter(Boolean).length;
   const completionText = `(${completeFields}/${totalFields})`;
-  return (<div className="p-6">
+  return (
+    <div className="p-6">
       <div className="flex items-center justify-between">
         <div className="flex flex-col gap-y-2">
           <h1 className="text-2xl font-medium">Course setup</h1>
-          <span className="text-sm text-slate-700">Complete all fields {completionText}</span>
+          <span className="text-sm text-slate-700">
+            Complete all fields {completionText}
+          </span>
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mt-16">
         <div>
           <div className="flex items-center gap-x-2">
-            <IconBadge size="sm" icon={LayoutDashboard}/>
-            <h2 className="text-xl">
-              Customize your course
-            </h2>
+            <IconBadge size="sm" icon={LayoutDashboard} />
+            <h2 className="text-xl">Customize your course</h2>
           </div>
+          <TitleForm initialData={course} courseId={course.id} />
         </div>
       </div>
-    </div>);
+    </div>
+  );
 };
 export default CourseIdPage;
