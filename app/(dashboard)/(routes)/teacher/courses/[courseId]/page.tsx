41d6d8767a32aada1351c8da4e660aafc142f488
Rasoulmadani <ali.rahbary@chmail.ir>
2023-11-29 10:22:52 +0330

    create icon components with cva and add this to the course detail page

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,5 +1,50 @@
-const CourseIdPage = ({params}:{params:{courseId:string}}) => {
-    return ( <div>allah : {params.courseId}</div> );
-}
- 
+import { IconBadge } from "@/components/IconBadge";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { LayoutDashboard } from "lucide-react";
+import { redirect } from "next/navigation";
+
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+  const { userId } = auth();
+  if (!userId) {
+    redirect("/");
+  }
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+  });
+  if (!course) {
+    return redirect("/");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
+  const totalFields = requiredFields.length;
+  const completeFields = requiredFields.filter(Boolean).length;
+  const completionText = `(${completeFields}/${totalFields})`;
+  return (<div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-2xl font-medium">Course setup</h1>
+          <span className="text-sm text-slate-700">Complete all fields {completionText}</span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-2 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge size="sm" icon={LayoutDashboard}/>
+            <h2 className="text-xl">
+              Customize your course
+            </h2>
+          </div>
+        </div>
+      </div>
+    </div>);
+};
 export default CourseIdPage;
